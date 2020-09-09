e4a13b8fe7211a940c98f44d75bfe9f2833592be
Pedro_Soto <pedrosotto717@gmail.com>
2020-09-09 19:10:11 -0400

    Creado y maquetado el Header con el Slider, nav-bar, search-bar y main-menu
    * Maquetado del Header (Logo, search-bar, nav-bar)
    	** Creado el logo de la pagina '.svg' y la clase 'logo-px' para renderizarlo
    	** Creacion y estilos de la barra de Busqueda
    	** Configurados los estilos para el modo Busqueda '.is-search-h' y '.is-search'
    
    * Maquetado el Titulo Principal y el search-bar del centro del slider
    
    * Disenado el main-menu para dispositivos responsive v1
    
    * Diseno Responsive del header,logo,nav-bar,search-bar,(main-menu v1)
    
    * Estilos genericos para botones

---
+++ main.js
@@ -0,0 +1,17 @@
+import slidePX from "./js/slide_px.js"
+
+const slide = new slidePX({
+  container: ".slide",
+  items: ".slide__item",
+  active: "is-active",
+  activeOld: "is-active--prev",
+  time: 6000,
+  noMobile: true
+})
+
+slide.observer().run()
+
+document.querySelector(".btn-menu").addEventListener("click", ev =>{
+	console.log("Active")
+	document.querySelector(".menu-main").classList.toggle("is-active-menu")
+})
