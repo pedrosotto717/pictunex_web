8028554a9ae7fb8ce56f216740b5b3b143b545d3
pedro <pedrosotto717@gmail.com>
2020-09-18 19:47:50 -0400

    Ultimos Ajustes al Masonry Layout para Imagenes Estaticas

--- assets/js/modules/masonry-px.js
+++ assets/js/modules/masonry-px.js
@@ -1,27 +1,18 @@
-
-const ColectIMG = []
-
 function msr (containerSelector, itemsSelector, GRheight) {
     const items = document.querySelectorAll(itemsSelector),
-            container = document.querySelector(containerSelector),
             imgItems = [...document.querySelectorAll('.masonry-item img')]
 
     for (let i = 0; i < items.length; i++) {
 
         const H = imgItems[i].height 
-        const W = imgItems[i].width 
-        const newIMG = document.createElement("img")
-        newIMG.src = imgItems[i].src
-        ColectIMG.push(newIMG)
-
-        console.log(H,Math.round((H) / GRheight),Math.round( ((H / 2) / GRheight) ),W,items[i])
+        // console.log(H,Math.round((H) / GRheight),Math.round( ((H / 2) / GRheight) ),W,items[i])
 
         let hFinal = 1
 
         if(H<500){
-            hFinal = Math.round(H / GRheight)
+            hFinal = Math.ceil(H / GRheight)
         }else if(H>499 && H<1000){
-            hFinal = Math.round( ((H / 2) / GRheight) )
+            hFinal = Math.ceil( ((H / 2) / GRheight) )
         }else if(H>999){
             hFinal = Math.round( ((H / 3) / GRheight) )
         }
@@ -33,9 +24,6 @@ function msr (containerSelector, itemsSelector, GRheight) {
     }
 }
 
-console.log('HEY')
-
 addEventListener("load", ev => {
     msr('.masonry-layout', '.masonry-item', 100)
-    console.log("Inicia")
 })	
